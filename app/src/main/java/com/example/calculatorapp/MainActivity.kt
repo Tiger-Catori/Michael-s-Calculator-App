dc3fe944ccc70c5195ab5f3f1e91bc7f3ec1083c
Tiger-Catori <michaeladeleye365@gmail.com>
2022-06-22 09:15:07 +0100

    Update 09:14 22/06/2022

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import com.example.calculatorapp.R
+//import com.example.calculatorapp.R
 import com.example.kotlincalculatorapp.R
 import kotlinx.android.synthetic.main.activity_main.*
 
